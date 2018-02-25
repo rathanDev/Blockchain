9c4b2b5e2bab7bba3b2e4b4c58ef7df502549570
janarthan <janarthan@finnovation.lk>
2018-02-25 14:57:20 +0530

    Add StringConversion sample contract

---
+++ sampleContracts/StringConversion.sol
@@ -0,0 +1,36 @@
+contract StringConversion {
+
+    function toBytes(string _content) public returns(bytes32) {
+        bytes32 inBytes = stringToBytes32(_content);
+        return inBytes;
+    }
+
+    function stringToBytes32(string memory source) public returns (bytes32 result) {
+        bytes memory tempEmptyStringTest = bytes(source);
+        if (tempEmptyStringTest.length == 0) {
+            return 0x0;
+        }
+
+        assembly {
+            result := mload(add(source, 32))
+        }
+    }
+
+    function bytes32ToString(bytes32 x) constant returns (string) {
+        bytes memory bytesString = new bytes(32);
+        uint charCount = 0;
+        for (uint j = 0; j < 32; j++) {
+            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
+            if (char != 0) {
+                bytesString[charCount] = char;
+                charCount++;
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(charCount);
+        for (j = 0; j < charCount; j++) {
+            bytesStringTrimmed[j] = bytesString[j];
+        }
+        return string(bytesStringTrimmed);
+    }
+
+}
