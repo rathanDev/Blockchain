83fb0196c489ac1591a1df46550c2dc4717c6afb
janarthan <janarthan@finnovation.lk>
2018-03-04 16:45:00 +0530

    Add deposit() and withdraw()

--- vladWulf/contracts/PayableContract.sol
+++ vladWulf/contracts/PayableContract.sol
@@ -2,12 +2,22 @@ pragma solidity ^0.4.20;
 
 contract PayableContract {
     
+    address client;
+    bool _switch = false;
+
     function PayableContract() {
-        
+        client = msg.sender;
     }
-    
-    function receiveFunds() payable {
-        
+
+    function depositFunds() payable {
+    }
+
+    function withdrawFunds(uint amount) {
+        if (client.send(amount) ) {
+            _switch = true;
+        } else {
+            _switch = false;
+        }
     }
     
     function getBalance() view returns(uint) {
