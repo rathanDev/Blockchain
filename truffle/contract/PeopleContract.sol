8454774e290a7645a3780f483c25d611a3fc0289
janarthan <janarthan@finnovation.lk>
2018-03-06 12:16:18 +0530

    Update

--- truffle/contract/PeopleContract.sol
+++ truffle/contract/PeopleContract.sol
@@ -8,30 +8,41 @@ contract People {
     struct Person {
         uint id;
         bytes32 name;
-        uint[] languages;
+        Language[] languages;
         Status status;
     }
 
-    uint universalId = 0;
+    uint universalId = 1;
 
     mapping(address => Person) personMap;
     address[] public codes;
 
     function setPerson(bytes32 _name, uint[] _languages) public {
-        Person memory person;
+        Person storage person = personMap[msg.sender];
         person.id = universalId ++;
         person.name = _name;
-        person.languages = _languages;
+        person.languages = uintsToLanguages(_languages);
         person.status = Status.ACTIVE;
-        personMap[msg.sender] = person;
         codes.push(msg.sender);
     }
-    function getPerson() public view returns(uint, bytes32, uint[]) {
+    function getPerson() public view returns(uint, bytes32, Language[], Status) {
         Person storage person = personMap[msg.sender];
-        return(person.id, person.name, person.languages);
+        return(person.id, person.name, person.languages, person.status);
     }
 
-    function uintToLanguage(uint _language) public pure returns(string) {
+    function uintToLanguage(uint _uint) public pure returns(Language) {
+        if(_uint == uint(Language.TAMIL)) {
+            return Language.TAMIL;
+        }
+        if(_uint == uint(Language.ENGLISH)) {
+            return Language.ENGLISH;
+        }
+        if(_uint == uint(Language.SINHALA)) {
+            return Language.SINHALA;
+        }
+        return Language.OTHER;
+    }
+    function uintToLanguageString(uint _language) public pure returns(string) {
         if(_language == uint(Language.TAMIL)) {
             return "TAMIL";
         }
@@ -42,6 +53,14 @@ contract People {
             return "SINHALA";
         }
         return "OTHER";
+    }
+
+    function uintsToLanguages(uint[] _uints) public view returns(Language[]) {
+        Language[] languages;
+        for(uint i=0; i<_uints.length; i++) {
+            languages.push(uintToLanguage(_uints[i]));
+        }
+        return languages;
     }
 
     function getUniversalId() public view returns(uint) {
