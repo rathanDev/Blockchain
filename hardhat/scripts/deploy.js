12c8f91f4cd2334707e52e26e0e21306b61dbcf8
jana <rathan.kparam@gmail.com>
2023-04-26 08:23:06 +0800

    Try hardhat

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -6,39 +6,27 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-const main = async () => {
+async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
 
   const lockedAmount = hre.ethers.utils.parseEther("0.001");
 
-  const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transactions = await Transactions.deploy(unlockTime, { value: lockedAmount });
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await transactions.deployed();
+  await lock.deployed();
 
   console.log(
     `Lock with ${ethers.utils.formatEther(
       lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${transactions.address}`
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
   );
-};
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-
-const runMain = async () => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
-
-runMain();
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
